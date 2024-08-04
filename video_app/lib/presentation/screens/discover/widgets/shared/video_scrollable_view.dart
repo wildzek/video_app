9b4ba81028d84ea046040c834fd6d99f6f5c0a49
wildzek <dev@rogertorrent.com>
2024-08-04 23:08:32 +0200

    Like and views button configuration

--- video_app/lib/presentation/screens/discover/widgets/shared/video_scrollable_view.dart
+++ video_app/lib/presentation/screens/discover/widgets/shared/video_scrollable_view.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:video_app/domain/entities/video_post.dart';
+import 'package:video_app/presentation/screens/discover/widgets/shared/video_buttons.dart';
 
-class VideoScrollableView extends StatelessWidget{
-    
+class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
 
   const VideoScrollableView({super.key, required this.videos});
 
-
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container( color: Colors.red),
-        Container( color: Colors.blue),
-        Container( color: Colors.yellow),
-        Container( color: Colors.green),
-        Container( color: Colors.white),
-        Container( color: Colors.purple),
-        Container( color: Colors.grey),
-        Container( color: Colors.black),
-      ],
-    
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost)
+              
+              
+            ),
+
+
+          ],
+        );
+      },
     );
   }
 }
