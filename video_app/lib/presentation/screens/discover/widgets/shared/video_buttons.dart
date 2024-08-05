15d66029b24466a63ad75528b36245ac7411b0c0
wildzek <dev@rogertorrent.com>
2024-08-05 18:45:11 +0200

    animate_do package installed. Animation for play button created

--- video_app/lib/presentation/screens/discover/widgets/shared/video_buttons.dart
+++ video_app/lib/presentation/screens/discover/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:video_app/config/theme/helpers/human_formats.dart';
 import '../../../../../domain/entities/video_post.dart';
@@ -12,8 +13,16 @@ class VideoButtons extends StatelessWidget {
       children: [
         _CustomIconButton(
             value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
+        const SizedBox(height: 20),
         _CustomIconButton(     
             value: video.views, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(     
+              value: 0, iconData: Icons.play_circle_outline),
+        ),     
       ],
     );
   }
