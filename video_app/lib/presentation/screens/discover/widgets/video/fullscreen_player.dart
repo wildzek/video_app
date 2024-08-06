6d8df02332b1ad7a68237fd69077fbcf6ef76ed4
wildzek <dev@rogertorrent.com>
2024-08-06 20:23:41 +0200

    Video player crashes with local video data. Video caption created inside the player.

--- video_app/lib/presentation/screens/discover/widgets/video/fullscreen_player.dart
+++ video_app/lib/presentation/screens/discover/widgets/video/fullscreen_player.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:video_app/shared/data/local_video_post.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -34,10 +36,48 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: controller.initialize(),
-      builder: (context, snapshot) {
-        return const Center(child: CircularProgressIndicator(strokeWidth: 2, color: Colors.red));
-      },
+        future: controller.initialize(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(
+                child: CircularProgressIndicator(
+                    strokeWidth: 2, color: Colors.red));
+        }
+
+        return AspectRatio(
+          aspectRatio: controller.value.aspectRatio, 
+          child: Stack(
+            children: [
+              VideoPlayer(controller),
+
+              Positioned(
+                bottom: 50,
+                left: 20,
+                child: _VideoCaption(caption: widget.caption)
+              ),
+            ]
+          )
+        );
+      }
+    );
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+
+  final String caption;
+
+  const _VideoCaption({super.key, required this.caption});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(caption, maxLines: 2, style: titleStyle)
     );
   }
 }
