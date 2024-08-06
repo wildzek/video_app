25b9ce90f32cb3963d75296f98a8135dbc9d6760
wildzek <dev@rogertorrent.com>
2024-08-06 19:44:09 +0200

    VideoPlayer configured. Progress indicator enabled

--- video_app/lib/presentation/screens/discover/widgets/video/fullscreen_player.dart
+++ video_app/lib/presentation/screens/discover/widgets/video/fullscreen_player.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class FullScreenPlayer extends StatelessWidget {
+class FullScreenPlayer extends StatefulWidget {
   final String videoUrl;
   final String caption;
 
-  const FullScreenPlayer({super.key, required this.videoUrl, required this.caption});
+  const FullScreenPlayer(
+      {super.key, required this.videoUrl, required this.caption});
+
+  @override
+  State<FullScreenPlayer> createState() => _FullScreenPlayerState();
+}
+
+class _FullScreenPlayerState extends State<FullScreenPlayer> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = VideoPlayerController.asset(widget.videoUrl)
+      ..setVolume(0)
+      ..setLooping(true)
+      ..play();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: controller.initialize(),
+      builder: (context, snapshot) {
+        return const Center(child: CircularProgressIndicator(strokeWidth: 2, color: Colors.red));
+      },
+    );
   }
 }
